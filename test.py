1a743e6de83912de4b810d6d66c9e42cdd608b07
abejfehr <abe.fehr@gmail.com>
2014-10-09 22:33:59 -0400

    testing certain functions

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 from sudokusolver import *
 
-f = open("msk_009.txt", "r")
-for line in f.readlines():
-  print solve_sudoku(line.strip())
-f.close()
+p = ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8"
+
+print get_candidate_rows(p)
+print " "*25
+print get_candidate_cols(p)
+print " "*25
+print get_candidate_boxes(p)
+
+#f = open("msk_009.txt", "r")
+#for line in f.readlines():
+#  print solve_sudoku(line.strip())
+#f.close()
